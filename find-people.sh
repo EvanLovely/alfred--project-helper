e170fdca5a30cd19471ec30edad7b5ae250041a5
Evan Lovely <evan@evanlovely.com>
2014-03-20 20:07:13 -0700

    Added People Functionality

---
+++ find-people.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+OUTPUTSTRING="<items>
+"
+if [ "$1" ] 
+  then
+  TO_SEARCH=$(find ~/active-project/files/people -type d -depth 1 -iname *$1*)
+  else
+  TO_SEARCH=$(find ~/active-project/files/people -type d -depth 1)
+
+  OUTPUTSTRING="$OUTPUTSTRING
+  <item>
+    <title>People of this Project:</title>
+    <subtitle>Return: copy email ~ Cmd+Return: Edit info ~ Cmd+Y: Big Photo</subtitle>
+  </item>"
+
+fi
+FILES=${TO_SEARCH// /_}
+
+
+for i in $FILES; do
+    path="${i//_/ }"
+    folder=`echo $path | sed "s/.*\///" | sed "s/\.txt//"`
+    contents=`cat "$path/info.txt"`
+    OUTPUTSTRING="$OUTPUTSTRING
+    <item arg='$path/photo.jpg' type='file'>
+      <title>$folder</title>
+      <subtitle><![CDATA[$contents]]></subtitle>
+      <icon>$path/photo.jpg</icon>
+      </item>"
+#     foldername=${i##*/};
+#     file="$i";
+    # echo "e $i d";
+done
+
+OUTPUTSTRING="$OUTPUTSTRING
+</items>"
+
+echo "$OUTPUTSTRING"
