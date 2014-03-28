ede573eab9efa1f99345ecf37d9cc6711a30c3ea
Evan Lovely <evan@evanlovely.com>
2014-03-28 08:42:49 -0700

    Fixed Asset Script Filter

--- find-assets.sh
+++ find-assets.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
 dir=~/active-project/files/assets/
-OUTPUTSTRING="<items>
-"
+
+echo "<items>"
+
 if [ "$1" ] 
   then
-  # Filter List
-
-  # Find all files that match, except `.DS_Store`
-  args="${1// /*}" # swap spaces for *
-  TO_SEARCH=$(find "$dir" -type f -iname "*$args*" | grep -iv "DS_Store" | grep -iv "Icon" )
+    # Filter List
+    mdfind -0 -onlyin "$dir" "$1" | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle>In: `dirname "${1/$HOME/~}"`</subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
   else
-  # List All
-  TO_SEARCH=$(find "$dir" -type f | grep -iv "Icon" | grep -iv "DS_Store")
+    # List All
+    find "$dir" \! -name ".DS_Store" \! -name "Icon?" -depth 2 -type f -print0 | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle>In: `dirname "${1/$HOME/~}"`</subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+
 fi
 
-# Replacing any spaces with seperator b/c the below `for` loop is cycling on spaces
-seperator="☀"
-FILES=${TO_SEARCH// /$seperator}
-
-for i in $FILES; do
- 
-    # Let's get our spaces we took away back by swapping it for the seperator
-    path="${i//$seperator/ }"
-
-    # Extract filename from path. remove everything up to last `/`.`
-    file=`echo ${path/$dir\//}`
-
-    OUTPUTSTRING="$OUTPUTSTRING
-    <item arg='$path' type='file' valid='yes' uid='$path'>
-      <title>$file</title>
-      <icon type='fileicon'>$path</icon>
-      </item>"
-#     foldername=${i##*/};
-#     file="$i";
-    # echo "e $i d";
-done
-
-OUTPUTSTRING="$OUTPUTSTRING
-</items>"
-
-echo "$OUTPUTSTRING"
+echo "</items>"
