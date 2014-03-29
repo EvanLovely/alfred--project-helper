4e991b45f01a36675d4ea50e1eaffe325fc8b504
Evan Lovely <evan@evanlovely.com>
2014-03-28 20:36:07 -0700

    Consolidating Script Filters into new generalized, faster format

--- search-for-files.sh
+++ search-for-files.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
-dir=~/active-project/files/assets/
+# $1: directory
+# $2: search terms
 
 echo "<items>"
 
-if [ "$1" ] 
+if [ "$2" ] 
   then
     # Filter List
-    mdfind -0 -onlyin "$dir" "$1" | xargs -0 -I {} \
+    mdfind -0 -onlyin "$1" "$2" | xargs -0 -I {} \
       sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle>In: `dirname "${1/$HOME/~}"`</subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
   else
     # List All
-    find "$dir" \! -name ".DS_Store" \! -name "Icon?" -depth 2 -type f -print0 | xargs -0 -I {} \
+    find "$1" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f -print0 | xargs -0 -I {} \
       sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle>In: `dirname "${1/$HOME/~}"`</subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
 fi
