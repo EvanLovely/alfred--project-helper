c6e3dae32acb484e317fd4eab981740e2bb3e396
Evan Lovely <evan@evanlovely.com>
2014-04-06 17:42:48 -0700

    Added way to Add Link with Tags. Improved Snippet addition by being able to set Tags.

--- find-links.sh
+++ find-links.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
-dir=~/active-project/files/links
-OUTPUTSTRING="<items>
-"
+dir=~/active-project/files/links/
+echo "<items>"
+
 if [ "$1" ] 
   then
-  # Filter List
-
-  # Find all files that match, except `.DS_Store`, and add them to a new text file
-  args="${1// /*}" # swap spaces for *
-  TO_SEARCH=$(find $dir -type f -iname "*$args*" | grep -iv "DS_Store")
+    # Filter List
+    mdfind -0 -onlyin "$dir" "$1 NOT kind:folder" | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
   else
-  # List All
-  TO_SEARCH=$(find $dir -type f -not -iname .DS_Store)
+    # List All
+    find "$dir" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f -print0 | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><icon type=\"fileicon\">$1</icon></item>"' -- {}
+
 fi
 
-# Replacing any spaces with _ b/c the below `for` loop is cycling on spaces
-seperator="☀"
-FILES=${TO_SEARCH// /$seperator}
-
-for i in $FILES; do
- 
-    # Let's get our spaces we took away back by swapping it for the seperator
-    path="${i//$seperator/ }"
-
-    # Extract filename from path. 1st sed: remove everything up to last `/`. 2nd sed: remove `.txt` @todo yank all file extensions, not just just `.txt`
-    file=`echo ${path/$dir\//} | sed "s/\.txt//"`
-
-    OUTPUTSTRING="$OUTPUTSTRING
-    <item arg='$path' type='file' uid='$path'>
-      <title>$file</title>
-      <icon type='fileicon'>$path</icon>
-      </item>"
-#     foldername=${i##*/};
-#     file="$i";
-    # echo "e $i d";
-done
-
-OUTPUTSTRING="$OUTPUTSTRING
-</items>"
-
-echo "$OUTPUTSTRING"
+echo "</items>"
--- find-snippets.sh
+++ find-snippets.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-# $1: directory
-# $2: search terms
+export dir="$(cd $1 && pwd -P)"
+# @todo:
+# - Show Spotlight Comments without big lag - `$(mdls -name kMDItemFinderComment -raw "$1")`
+# - Have search be able to look in folder titles (`kMDItemPath` doesn't let you query against it: http://stackoverflow.com/questions/1341590/no-results-in-spotlight-in-searches-against-kmditempath)
 
 echo "<items>"
 
 if [ "$2" ] 
   then
     # Filter List
-    mdfind -0 -onlyin "$1" "$2 NOT kind:folder" | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><subtitle><![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+    mdfind -0 -onlyin "$dir" "$2 NOT kind:folder" | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(echo "${1/$dir\/}")</title><subtitle>$(/opt/local/bin/tag -Nl "$1") ~ <![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
   else
     # List All
-    find "$1" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f -print0 | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><subtitle><![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+    find "$dir" \( -name .git -o -name .hg -o -name ".DS_Store" -o -name "Icon?" \) -prune -o \( -type f -print0 \) | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(echo "${1/$dir\/}")</title><subtitle>$(/opt/local/bin/tag -Nl "$1") ~ <![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
 fi
 
