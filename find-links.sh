1ded85f38b6a18e435b098a0330f1f4cdd4f9636
Evan Lovely <evan@evanlovely.com>
2014-04-06 18:16:24 -0700

    Improved Link Views

--- find-links.sh
+++ find-links.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-dir=~/active-project/files/links/
+dir=~/active-project/files/links
+export dir="$(cd $dir && pwd -P)"
 echo "<items>"
 
 if [ "$1" ] 
   then
     # Filter List
     mdfind -0 -onlyin "$dir" "$1 NOT kind:folder" | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><icon type=\"fileicon\">$1</icon></item>"' -- {}
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(echo "${1/$dir\/}" | sed "s/.webloc//")</title><subtitle>$(/opt/local/bin/tag -Nl "$1") ~ <![CDATA[$(grep "string" "$1" | sed "s/<string>//" | sed "s/<\/string>//")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
   else
     # List All
-    find "$dir" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f -print0 | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(basename "$1")</title><icon type=\"fileicon\">$1</icon></item>"' -- {}
+    find "$dir" \( -name .git -o -name .hg -o -name ".DS_Store" -o -name "Icon?" \) -prune -o \( -type f -print0 \) | xargs -0 -I {} \
+      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>$(echo "${1/$dir\/}" | sed "s/.webloc//")</title><subtitle>$(/opt/local/bin/tag -Nl "$1") ~ <![CDATA[$(grep "string" "$1" | sed "s/<string>//" | sed "s/<\/string>//")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
 
 fi
 
