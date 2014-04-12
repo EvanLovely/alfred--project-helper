3630a01b548da8a83879d2dea3a904662e87a72d
Evan Lovely <evan@evanlovely.com>
2014-04-11 18:27:02 -0700

    Improvements and fixes to the find by tags filter

--- find-tagged.sh
+++ find-tagged.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+IFS=$'\n'
+tag="${1%%➢*}"
+filter="${1##*➢}"
 echo "<items>"
 
-mdfind -0 "tag:$1" | xargs -0 -I {} \
-  sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>${1##*/}</title><subtitle>${1/$HOME/~}</subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+for i in $(find -x ~/active-project -follow -exec /opt/local/bin/tag -m "$tag" {} +\; | egrep -i "${filter// /.*}"); do
+  echo "<item><title>${i##*/}</title><subtitle>$(echo "${i/$HOME/~}" | sed "s/${i##*/}//")</subtitle><icon>$i</icon></item>"
+done
 
 echo "</items>"
+
+unset IFS
