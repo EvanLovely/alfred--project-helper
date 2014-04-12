4d3d71fcb35b964b02fd790750a32740a6e95a09
Evan Lovely <evan@evanlovely.com>
2014-04-11 18:30:11 -0700

    Fixes for tagged files action

--- find-tagged.sh
+++ find-tagged.sh
@@ -5,7 +5,7 @@ filter="${1##*➢}"
 echo "<items>"
 
 for i in $(find -x ~/active-project -follow -exec /opt/local/bin/tag -m "$tag" {} +\; | egrep -i "${filter// /.*}"); do
-  echo "<item><title>${i##*/}</title><subtitle>$(echo "${i/$HOME/~}" | sed "s/${i##*/}//")</subtitle><icon>$i</icon></item>"
+  echo "<item type=\"file\" arg=\"$i\"><title>${i##*/}</title><subtitle>$(echo "${i/$HOME/~}" | sed "s/${i##*/}//")</subtitle><icon>$i</icon></item>"
 done
 
 echo "</items>"
