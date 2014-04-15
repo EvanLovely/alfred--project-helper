47f400d2824725dc43751780bffc8d5107a2b6d1
Evan Lovely <evan@evanlovely.com>
2014-04-15 03:22:53 -0700

    Improving Scripts more

--- scripts.sh
+++ scripts.sh
@@ -8,13 +8,21 @@ echo "<items>"
 if [ "$args" ] 
   then
     # Filter List
-    mdfind -0 -onlyin "$dir" "$args" | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle><![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+    IFS=$'\n'
+    for i in $(mdfind -onlyin "$dir" "$args"); do
+        echo "<item arg=\"$i\" type=\"file\" uid=\"$i\"><title>`basename "$i"`</title><subtitle><![CDATA[$(cat "$i")]]></subtitle><icon type=\"fileicon\">$i</icon></item>"
+    done
+    unset IFS
 
   else
     # List All
-    find "$dir" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f -print0 | xargs -0 -I {} \
-      sh -c 'echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title><subtitle><![CDATA[$(cat "$1")]]></subtitle><icon type=\"fileicon\">$1</icon></item>"' -- {}
+
+    echo "<item><title>Add a New Script with Cmd+Enter</title><subtitle>Enter on a script runs it in a new Terminal tab; Option+Enter runs it in the background</subtitle></item>"
+    IFS=$'\n'
+    for i in $(find "$dir" \! -name ".DS_Store" \! -name "Icon?" -maxdepth 2 -type f); do
+        echo "<item arg=\"$i\" type=\"file\"><title>`basename "$i"`</title><subtitle><![CDATA[$(cat "$i")]]></subtitle><icon type=\"fileicon\">$i</icon></item>"
+    done
+    unset IFS
 
 fi
 
