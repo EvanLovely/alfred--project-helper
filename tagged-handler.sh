a6f257b50422e9cd224d351019fd577c4e97d236
Evan Lovely <evan@evanlovely.com>
2014-04-21 11:52:44 -0700

    Adding current project workaround for tag search results too

--- tagged-handler.sh
+++ tagged-handler.sh
@@ -1,6 +1,10 @@
 arg="$1"
 if [[ "$arg" == */snippets/* ]]; then
   sh snippets-handler.sh "$arg"
+elif [[ "$arg" == *theme-copies* ]]; then
+  open "$(echo "$arg" | sed "s,^.*theme-copies,$HOME/active-project/theme__on_server,")"
+elif [[ "$arg" == *module-copies* ]]; then
+  open "$(echo "$arg" | sed "s,^.*module-copies,$HOME/active-project/modules__on_server,")"
 else
   open "$arg"  
 fi
