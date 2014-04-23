1f0c42d2a712bad447c1e5157180ba66cadb01f3
Evan Lovely <evan@evanlovely.com>
2014-04-23 05:42:14 -0700

    Foundation of menu

---
+++ menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "<items>"
+if [[ "$1" == "" ]]; then
+
+echo "<item><title>Find</title></item>"
+echo "<item autocomplete='settings' arg='settings'><title>Settings</title></item>"
+fi
+
+if [[ "$1" == "settings" ]]; then
+  echo "<item autocomplete='settings 1' arg='settings 1'><title>Setting 1</title></item>"
+  echo "<item autocomplete='settings 2' arg='settings 2'><title>Setting 2</title></item>"
+fi
+if [[ "$1" == "settings "* ]]; then
+  echo "<item autocomplete='settings 1a' arg='settings 1a'><title>Setting 1a</title></item>"
+  echo "<item autocomplete='settings 2a' arg='settings 2a'><title>Setting 2a</title></item>"
+    
+fi    
+echo "</items>"
