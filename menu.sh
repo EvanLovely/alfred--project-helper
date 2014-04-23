a31742918477a9bd9f7990154ffa76a22ec1ec9d
Evan Lovely <evan@evanlovely.com>
2014-04-23 05:55:24 -0700

    Functioning nested menu

--- menu.sh
+++ menu.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 echo "<items>"
-if [[ "$1" != " " ]]; then
+if [[ "$1" != "go"* ]]; then
 
 echo "<item><title>Find</title></item>"
-echo "<item autocomplete='settings' arg='settings'><title>Settings</title></item>"
+echo "<item autocomplete='go settings' valid='no'><title>Settings</title></item>"
 fi
 
-if [[ "$1" == "settings " ]]; then
-  echo "<item autocomplete='settings 1' arg='settings 1'><title>    Setting 1</title></item>"
-  echo "<item autocomplete='settings 2' arg='settings 2'><title>    Setting 2</title></item>"
+if [[ "$1" == "go settings"* ]]; then
+  echo "<item autocomplete='go 1 setting' arg='go 1 setting' valid='no'><title>Setting 1</title></item>"
+  echo "<item autocomplete='go 2 setting' arg='go 2 setting' valid='no'><title>Setting 2</title></item>"
 fi
-if [[ "$1" == "settings 2"* ]]; then
-  echo "<item autocomplete='settings 1a' arg='settings 1a'><title>        Setting 2a</title></item>"
-  echo "<item autocomplete='settings 2a' arg='settings 2a'><title>        Setting 2b</title></item>"
+if [[ "$1" == "go 1 setting"* ]]; then
+  echo "<item autocomplete='settings 1a' arg='settings 1a' valid='no'><title>Setting 1b</title></item>"
+  echo "<item autocomplete='settings 1b' arg='settings 1b' valid='no'><title>Setting 1b</title></item>"
+    
+fi    
+if [[ "$1" == "go 2 setting"* ]]; then
+  echo "<item autocomplete='settings 1a' arg='settings 2a' valid='no'><title>Setting 2a</title></item>"
+  echo "<item autocomplete='settings 2b' arg='settings 2b' valid='no'><title>Setting 2b</title></item>"
     
 fi    
 echo "</items>"
