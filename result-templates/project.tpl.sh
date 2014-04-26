8818c38fab5005d41cf2de8fb4bc5695306d4db1
Evan Lovely <evan@evanlovely.com>
2014-04-26 01:54:51 -0700

    putting project switcher in menu

--- result-templates/project.tpl.sh
+++ result-templates/project.tpl.sh
@@ -1,1 +1,1 @@
-echo "<item arg=\"$1\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title></item>"
+echo "<item arg=\"sh switch-project.sh '$1'\" type=\"file\" uid=\"$1\"><title>`basename "$1"`</title></item>"
