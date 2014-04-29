8780485fbf4d1ad5ea4bd70a0099131cdc67c6e2
Evan Lovely <evan@evanlovely.com>
2014-04-29 19:42:47 -0400

    simplifying and fixing update process

--- update.sh
+++ update.sh
@@ -1,4 +1,1 @@
-rm ~/Downloads/Project\ Helper.alfredworkflow
-open -g https://github.com/EvanLovely/alfred--project-helper/blob/master/Project%20Helper.alfredworkflow?raw=true
-osascript -e 'delay 10'
-open ~/Downloads/Project\ Helper.alfredworkflow
+open https://github.com/EvanLovely/alfred--project-helper/blob/master/ProjectHelper.alfredworkflow?raw=true
