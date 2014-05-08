dd3ebccc2bb04c8cfc1547cdf00670484659d088
Evan Lovely <elovely@phase2technology.com>
2014-05-08 13:42:59 -0700

    trim copied links

--- result-templates/links.tpl.sh
+++ result-templates/links.tpl.sh
@@ -7,5 +7,5 @@ if [ "$2" == "--no-uid" ]; then
 fi
 echo "<title>$(echo "${1/$dir\/}" | sed "s/.webloc//")</title>"
 echo "<subtitle>$(/opt/local/bin/tag -Nl "$1") ~ <![CDATA[$link]]></subtitle>"
-echo "<text type=\"copy\">$link</text>"
+echo "<text type=\"copy\">$(echo $link | tr -d ' ')</text>"
 echo "<icon type=\"fileicon\">$1</icon></item>"
